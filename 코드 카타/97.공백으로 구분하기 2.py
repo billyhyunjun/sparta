4a817925fc39b9089fdeb03c640a165856de9ce6
Billyhyunjun <phjphj9999@gmail.com>
2024-03-19 10:03:51 +0900

    코드 카타 문제 풀이

---
+++ 코드 카타/97.공백으로 구분하기 2.py
@@ -0,0 +1,12 @@
+def solution(n_str):
+    answer = str(int(n_str))
+    return answer
+
+
+a = "0010"
+# b = "ef"
+# c = 3
+result = solution(a)
+# result = solution(a, b)
+# result = solution(a, b, c)
+print(result)
